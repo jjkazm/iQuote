b2609ca9927a1a1ad238bfe77822f888808ed88b
Kuba Kazmierczak <kazmierczak88@gmail.com>
2018-08-29 01:18:31 +0200

    add quotes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'quotes/index'
+  get 'quotes/new'
+  get 'index/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
