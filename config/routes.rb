7c3396e93ca266f4510cabfbbbf246573cfedba0
Kuba Kazmierczak <kazmierczak88@gmail.com>
2018-08-29 01:32:02 +0200

    add root site in route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'quotes/index'
   get 'quotes/new'
   resources :quotes
-  get '*path' => redirect('/') 
+
+  # go to this page if url is not found
+  get '*path' => redirect('/')
+
+  #set up landing page 
+  root to: 'posts#index'
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
