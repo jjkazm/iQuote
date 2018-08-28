b0ec436cc57a6a657c36f9aa060694454d09aeff
Kuba Kazmierczak <kazmierczak88@gmail.com>
2018-08-29 01:22:35 +0200

    config routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'quotes/index'
   get 'quotes/new'
-  get 'index/new'
+  resources :quotes
+  get '*path' => redirect('/') 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
