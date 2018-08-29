c9216d9fe83ec713dcabf94a99873611eaccde4f
Kuba Kazmierczak <kazmierczak88@gmail.com>
2018-08-29 16:21:40 +0200

    remove 2 seed records

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@
 		'Bond. James Bond.', 'Louis, I think this is the beginning of a beautiful friendship.', 
 		'I see dead people.', 'Houston, we have a problem.', 
 		'After all, tomorrow is another day!', 'Get your stinking paws off me, you damned dirty ape.',
-		'Is it safe?', 'My mother thanks you. My father thanks you. My sister thanks you. And I thank you.',
+		 'My mother thanks you. My father thanks you. My sister thanks you. And I thank you.',
 		'Listen to them. Children of the night. What music they make.', 'Hasta la vista, baby.',
 		'My precious.', 'Carpe diem. Seize the day, boys. Make your lives extraordinary.', 
 		'That rug really tied the room together.', 'When women go wrong, men go right after them.',
 		'This building has to be at least…. three times bigger than this!', 'Yeah, but I shoot with this hand.',
 		'Leave the gun. Take the cannoli.', 'Elementary, my dear Watson.',
-		'Is that all he said?', 'Excuse me. I believe you have my stapler.']
+		'Excuse me. I believe you have my stapler.']
 
 
 quotes_list.each do |quote|
