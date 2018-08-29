226e3d628f9defdc601e8bbb9c7ff8953d93bae8
Kuba Kazmierczak <kazmierczak88@gmail.com>
2018-08-29 03:41:05 +0200

    add routes in controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,20 @@
 class QuotesController < ApplicationController
-  def index
-  end
-
-  def new
-  end
+  # show all quotes
+   def index
+        @quote =Quote.all.order("created_at DESC")
+    end  
+    
+  # render new quote form
+    def new
+        @quote = Quote.new
+    end
+  # saves new quote in db
+    def create
+     @quote = Quote.new(quote_params)
+     if @quote.save
+          redirect_to quotes_path
+     else 
+          render ‘new’
+     end 
+    end
 end
